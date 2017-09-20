ff4be2ce97a3684f3282367741aac073408a1920
angieskazka <konzeptmeister@gmail.com>
2017-09-20 10:33:47 +0200

    Change URL to ola's example

--- src/assets/js/toolconfigs/BubbleChart-minimal.js
+++ src/assets/js/toolconfigs/BubbleChart-minimal.js
@@ -1,6 +1,6 @@
 var VIZABI_MODEL = {
   "data": {
     "reader": "csv",
-    "path": "https://raw.githubusercontent.com/vizabi/vizabi-preview/master/src/data/core_data_unpivoted_20170920a.csv"
+    "path": "https://raw.githubusercontent.com/olarosling/workbench/master/core_data_unpivoted.csv"
   }
 };
